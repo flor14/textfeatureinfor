d21fd1d547e1d4dbbdd3cf2aa3de27d167c61e6c
paradise1260 <paniz.fazlali@gmail.com>
2022-01-24 19:58:01 -0800

    Grouped the tests

--- tests/testthat/test-remove_stop_words.R
+++ tests/testthat/test-remove_stop_words.R
@@ -1,28 +1,16 @@
 test_that("remove_stop_words() removes stop words", {
-  expect_equal(remove_stop_words("We cannot solve our problems with the same thinking we used when we created them.")
+    expect_equal(remove_stop_words("We cannot solve our problems with the same thinking we used when we created them.")
                , c("solve", "thinking", "created"))
-})
-
-test_that("remove_stop_words() removes stop words", {
     expect_equal(remove_stop_words("!!!"), c("!!!"))
-})
-
-test_that("remove_stop_words() removes stop words", {
     expect_equal(remove_stop_words("UBC MDS is an amazing program!"), c("ubc", "mds", "amazing", "program!"))
 })
 
 test_that("remove_stop_words() errors if the type is not character", {
     expect_error(remove_stop_words(2))
-})
-
-test_that("remove_stop_words() errors if the type is not character", {
     expect_error(remove_stop_words(list('hello', 'world')))
 })
 
 test_that("remove_stop_words() errors if the type is empty", {
     expect_error(remove_stop_words(""))
-})
-
-test_that("remove_stop_words() errors if the type is empty", {
     expect_error(remove_stop_words(" "))
 })
