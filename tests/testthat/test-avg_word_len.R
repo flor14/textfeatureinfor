dd7a3d6ebfe269f24d129766e2f3ab6dc35aea1b
Kiran Phaterpekar <kphaterp@student.ubc.ca>
2022-01-25 15:37:00 -0800

    Add tests for error messages

--- tests/testthat/test-avg_word_len.R
+++ tests/testthat/test-avg_word_len.R
@@ -5,3 +5,9 @@ test_that("avg_word_len() finds the average length of the words in a string", {
     expect_equal(avg_word_len(""), 0)
     expect_equal(avg_word_len("!!words!!"), 5)
 }
+
+test_that("avg_word_len() throws an error when the type is not a string", {
+  expect_error(avg_word_len(4))
+  expect_error(avg_word_len(list("words", "in", "a", "list")))
+}
+
