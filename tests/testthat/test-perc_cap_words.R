7999032e48d939c8f93678a872e09d9e709c175f
jacqann <jacqueline.ann.chong@gmail.com>
2022-01-25 14:24:04 -0800

    Added Unit tests

---
+++ tests/testthat/test-perc_cap_words.R
@@ -0,0 +1,18 @@
+library(stringr)
+library(test_that)
+
+test_that("perc_cap_words() counts percentage of fully capitalised words in string", {
+    expect_equal(perc_cap_words("THIS IS SPAM!", "100"))
+    expect_equal(perc_cap_words("1234 !!! oked", "0"))
+    expect_equal(perc_cap_words("I have downloaded this package Before! YAHOO!"), "14.2857142857143")
+})
+
+test_that("perc_cap_words() throws an error if the type is not a string", {
+    expect_error(perc_cap_words(123))
+    expect_error(perc_cap_words(Greetings))
+})
+
+test_that("perc_cap_words() throws an error is the string is empty", {
+    expect_error(perc_cap_words(""))
+    expect_error(perc_cap_words(" "))
+})
