cd58867907675ae7765efa82950d7742e9009b47
Kiran Phaterpekar <kphaterp@student.ubc.ca>
2022-01-25 16:31:17 -0800

    Fix syntax issue with test script

--- tests/testthat/test-avg_word_len.R
+++ tests/testthat/test-avg_word_len.R
@@ -4,13 +4,13 @@ test_that("avg_word_len() finds the average length of the words in a string", {
     expect_equal(avg_word_len("!!"), 0)
     expect_equal(avg_word_len(""), 0)
     expect_equal(avg_word_len("!!words!!"), 5)
-}
+})
 
 test_that("avg_word_len() throws an error when the type is not a string", {
   expect_error(avg_word_len(4))
   expect_error(avg_word_len(list("words", "in", "a", "list")))
-}
+})
 
 test_that("avg_word_len() throws an error if there is more than 1 element in the character vector", {
   expect_error(avg_word_len(c("multiple", "words")))
-}
+})
