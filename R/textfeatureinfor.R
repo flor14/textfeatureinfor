1153082bdecf98fedd59faafbb6eabd6616e0740
Kiran Phaterpekar <kphaterp@student.ubc.ca>
2022-01-25 16:29:52 -0800

    Complete avgwordlen function body

--- R/textfeatureinfor.R
+++ R/textfeatureinfor.R
@@ -49,11 +49,21 @@ avg_word_len <- function(text) {
   # Get all punctuation from text
   punc <- extract_punct(text)
   
+  # removes all punctuation from string
   for (char in punc) {
     text <- str_replace(text, char, " ")
   }
+  # separates words by spaces and places words into a list
+  word_list <- as.list(unlist(strplit(x, '[[:space:]]')))
   
+  # calculates the average length of the words in the string
+  letter_count <- 0
+  for (word in word_list) {
+    letter_count <- letter_count + length(word)
+  }
+  average_length <- letter_count / length(word_list)
   
+  return (average_length)
 }
 
 
