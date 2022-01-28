18bd4e2cc657bb899db8f6a114aa11e62b3dc1c5
Kiran Phaterpekar <kphaterp@student.ubc.ca>
2022-01-28 10:51:20 -0800

    Add test for string with only spaces

--- tests/testthat/test-avg_word_len.R
+++ tests/testthat/test-avg_word_len.R
@@ -4,6 +4,7 @@ test_that("avg_word_len() finds the average length of the words in a string", {
     expect_equal(avg_word_len("!!"), 0)
     expect_equal(avg_word_len(""), 0)
     expect_equal(avg_word_len("!!words!!"), 5)
+    expect_equal(avg_word_len("          "), 0)
 })
 
 test_that("avg_word_len() throws an error when the type is not a string", {
